86dbbdc90d54bc0baa259f9fa9fa17d52fbdfd99
Allan Evans <allan@aelyssum.com>
2018-03-27 20:23:38 -0700

    Adjusted project structure

---
+++ AEBinaryTreeTests/BinaryTreeTests.swift
@@ -0,0 +1,170 @@
+//
+//  BinaryTreeTests.swift
+//  BinaryTreeTests
+//
+//  Created by Allan Evans on 8/25/16.
+//  Copyright © 2016 Aelyssum Corp. All rights reserved.
+//
+
+import XCTest
+@testable import AEBinaryTree
+
+class BinaryTreeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testCountAndDepth<T>(insert value: T, tree: BinaryTree<T>, expectedCount: Int, expectedDepth: Int) {
+        print("Inserting: \(value)")
+        tree.insert(value)
+        print(tree)
+        XCTAssert(tree.count == expectedCount, "Count should be \(expectedCount), got \(tree.count)")
+        XCTAssert(tree.isBalanced , "Tree unbalanced")
+    }
+
+    func testInsert0() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let tree = BinaryTree<Int>()
+        tree.insert(5)
+        tree.insert(3)
+        var expectedCount: Int!
+        var expectedDepth: Int!
+        expectedCount = 2
+        expectedDepth = 2
+        XCTAssert(tree.count == expectedCount, "Count should be \(expectedCount), got \(tree.count)\n\(tree)")
+        XCTAssert(tree.depth == expectedDepth, "Depth should be \(expectedDepth), got \(tree.depth)\n\(tree)")
+        print(tree)
+        tree.insert(1)
+        expectedCount = 3
+        expectedDepth = 2
+        XCTAssert(tree.count == expectedCount, "Count should be \(expectedCount), got \(tree.count)\n\(tree)")
+        XCTAssert(tree.depth == expectedDepth, "Depth should be \(expectedDepth), got \(tree.depth)\n\(tree)")
+        print(tree)
+    }
+
+    func test1() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let tree = BinaryTree<Int>()
+        tree.insert(5)
+        tree.insert(10)
+        tree.insert(3)
+        var expectedCount: Int!
+        var expectedDepth: Int!
+        expectedCount = 3
+        expectedDepth = 2
+        XCTAssert(tree.count == expectedCount, "Count should be \(expectedCount), got \(tree.count)\n\(tree)")
+        XCTAssert(tree.depth == expectedDepth, "Depth should be \(expectedDepth), got \(tree.depth)\n\(tree)")
+        print(tree)
+        tree.insert(1)
+        tree.insert(4)
+        expectedCount = 5
+        expectedDepth = 3
+        XCTAssert(tree.count == expectedCount, "Count should be \(expectedCount), got \(tree.count)\n\(tree)")
+        XCTAssert(tree.depth == expectedDepth, "Depth should be \(expectedDepth), got \(tree.depth)\n\(tree)")
+        print(tree)
+        tree.insert(0)
+        expectedCount = 6
+        expectedDepth = 3
+        XCTAssert(tree.count == expectedCount, "Count should be \(expectedCount), got \(tree.count)\n\(tree)")
+        XCTAssert(tree.depth == expectedDepth, "Depth should be \(expectedDepth), got \(tree.depth)\n\(tree)")
+        print(tree)
+        tree.insert(2)
+    }
+    
+    func test2() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let tree = BinaryTree<Int>()
+        testCountAndDepth(insert: 5, tree: tree, expectedCount: 1, expectedDepth: 1)
+        testCountAndDepth(insert: 15, tree: tree, expectedCount: 2, expectedDepth: 2)
+        testCountAndDepth(insert: 53, tree: tree, expectedCount: 3, expectedDepth: 2)
+        testCountAndDepth(insert: 68, tree: tree, expectedCount: 4, expectedDepth: 3)
+        testCountAndDepth(insert: 65, tree: tree, expectedCount: 5, expectedDepth: 3)
+        testCountAndDepth(insert: 83, tree: tree, expectedCount: 6, expectedDepth: 3)
+        testCountAndDepth(insert: 57, tree: tree, expectedCount: 7, expectedDepth: 3)
+//        tree.insert(74)
+    }
+    
+    func test3() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let tree = BinaryTree<Int>()
+        testCountAndDepth(insert: 0, tree: tree, expectedCount: 1, expectedDepth: 1)
+        testCountAndDepth(insert: 1, tree: tree, expectedCount: 2, expectedDepth: 2)
+        testCountAndDepth(insert: 2, tree: tree, expectedCount: 3, expectedDepth: 2)
+        testCountAndDepth(insert: 3, tree: tree, expectedCount: 4, expectedDepth: 3)
+        testCountAndDepth(insert: 4, tree: tree, expectedCount: 5, expectedDepth: 3)
+        testCountAndDepth(insert: 5, tree: tree, expectedCount: 6, expectedDepth: 3)
+        testCountAndDepth(insert: 6, tree: tree, expectedCount: 7, expectedDepth: 3)
+    }
+
+    func test4() {
+        let tree = BinaryTree<Int>()
+        let values = [6,5,4,3,2,1,0]
+        tree.insert(values)
+        print(tree)
+        XCTAssert(tree.isBalanced, "Tree unbalanced")
+        XCTAssert(tree.count == values.count , "Wrong count \(tree.count)")
+    }
+
+    func testRemove() {
+        let tree = BinaryTree<Int>()
+        let values = [6,5,4,3,2,1,0]
+        tree.insert(values)
+        print(tree)
+        tree.remove(3)
+        print(tree)
+        tree.remove(2)
+        print(tree)
+        tree.remove(values)
+        print(tree)
+        XCTAssert(tree.isBalanced, "Tree unbalanced")
+        XCTAssert(tree.isEmpty  , "Wrong count \(tree.count)")
+    }
+    
+    func testEnumerate() {
+        let tree = BinaryTree<Int>()
+        let values = [6,5,4,3,2,1,0]
+        tree.insert(values)
+        print(tree)
+        print(tree.enumerated)
+        XCTAssert(tree.enumerated == values.reversed(), "Enumeration failed")
+        tree.insert(7)
+        print(tree)
+        print(tree.enumerated)
+        XCTAssert(tree.enumerated[7] == 7, "Indexing failed")
+        XCTAssert(tree.enumerated[5] == 5, "Indexing failed")
+    }
+    
+    func testIndexing() {
+        
+        let tree = BinaryTree<Int>()
+        let values = [6,5,4,2,1,0]
+        tree.insert(values)
+        print(tree)
+        print(tree.enumerated)
+        XCTAssert(tree.index(of: 6) == 5, "Index 6 failed")
+        tree.insert(3)
+        XCTAssert(tree.index(of: 3) == 3, "Index 3 failed")
+        tree.insert(7)
+        XCTAssert(tree.index(of: 7) == 7, "Index 7 failed")
+
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # BinaryTree-Swift
-A BinaryTree or binary search tree conforming to the AVL (G.M. Adel’son-Vel’skii and E.M. Landis) requirement of self-balancing
+A BinaryTree or binary search tree conforming to the AVL (Adelson-Velsky and Landis) requirement of self-balancing where each subtree has left and right subtrees with depths (or heights) within ±1 of each other.
+
+Since many applications require sorting data as it arrives from some backend service, but then displaying it in the UI as an array, the BinaryTree class provides convenience methods for inserting elements and for providing a cached enumerated array property.  The enumerated property is only updated when accessed when invalidated, preventing an O(N) operation to update the UI each time an element is inserted or removed.
+
+Internal operation of each BinaryTree is thread-safe.  The BinaryTree class provides convenience methods for either synchronous or asynchronous operation, enabling insert() and remove() operations with O(logN) complexity, to be performed without blocking the main queue.
